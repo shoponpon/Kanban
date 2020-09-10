436f2f8634849e38b3dda454797660055cd63dee
shoponpon <shooishiworks@gmail.com>
2020-09-10 18:11:15 +0900

    404エラーを修正

---
+++ docs/precache-manifest.3d0dcacd1cde1df17331974bbc88f6ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631ec3f8e45d3ba71347d2aa9735f231",
+    "url": "/Kanban/index.html"
+  },
+  {
+    "revision": "e4733ceee37b1efd6858",
+    "url": "/Kanban/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "07ebdbbb719a1bca0c8a",
+    "url": "/Kanban/static/css/main.d968b0cf.chunk.css"
+  },
+  {
+    "revision": "e4733ceee37b1efd6858",
+    "url": "/Kanban/static/js/2.6283cee7.chunk.js"
+  },
+  {
+    "revision": "73b207d866c46f1507394f1c54d6ac97",
+    "url": "/Kanban/static/js/2.6283cee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ebdbbb719a1bca0c8a",
+    "url": "/Kanban/static/js/main.9e24e803.chunk.js"
+  },
+  {
+    "revision": "a073fc2d1e33dd3c3d7a",
+    "url": "/Kanban/static/js/runtime-main.342bd006.js"
+  }
+]);
